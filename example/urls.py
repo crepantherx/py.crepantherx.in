14d5bcdd4ba7130dcc06a99039dbcd559598d94a
juhidwivedi <dwivedijuhi01@gmail.com>
2024-10-08 16:35:39 +0530

    Release 1.3 Feature 1.1- page navigation

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,11 @@
 # example/urls.py
 from django.urls import path
 
-from example.views import index
+from example.views import index, project
+from . import views
 
 
 urlpatterns = [
     path('', index),
+    path('project/', project)
 ]
--- example/views.py
+++ example/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def project(request):
+    return render(request, 'project.html')
