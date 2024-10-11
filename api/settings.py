d24c9babef701b339d2083b8f96d5f7667605695
Sudhir.Singh <crepantherx@gmail.com>
2024-10-11 19:22:35 +0530

    release 1.0.11

--- api/settings.py
+++ api/settings.py
@@ -117,8 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, '/static/')
-# STATICFILES_DIRS = [BASE_DIR / 'static'] #dev
+# STATIC_URL = os.path.join(BASE_DIR, '/static/')
+
+STATIC_URL = "/var/www/py.crepantherx.in/static/"
+STATICFILES_DIRS = [BASE_DIR / 'static'] #dev
 # STATIC_ROOT = BASE_DIR / 'staticfiles'  #prod
 
 
