161d439f416ab5b2788595b03bbb49d90fb394dd
Sudhir.Singh <crepantherx@gmail.com>
2024-10-11 16:46:45 +0530

    release 1.0.10

--- api/settings.py
+++ api/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
+# STATICFILES_DIRS = [BASE_DIR / 'static'] #dev
+STATIC_ROOT = BASE_DIR / 'staticfiles'  #prod
 
 
 # Default primary key field type
